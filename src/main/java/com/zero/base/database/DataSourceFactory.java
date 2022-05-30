e0be55b64d90bc4d26ff45884cf9018078bb9729
chenlei <chenlei_1@moqipobing.com>
2022-05-30 18:49:11 +0800

    数据源的简单封装

--- src/main/java/com/zero/base/database/DataSourceFactory.java
+++ src/main/java/com/zero/base/database/DataSourceFactory.java
@@ -1,15 +1,36 @@
 package com.zero.base.database;
 
+import com.zaxxer.hikari.HikariDataSource;
+import com.zero.base.config.ConfigDict;
+
 import javax.sql.DataSource;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 数据源管理
  */
 public class DataSourceFactory {
 
+    private final ConfigDict configDict;
+    private final Map<DatabaseUrlKey, DataSource> dataSourceMap;
 
+    public DataSourceFactory(ConfigDict configDict) {
+        this.configDict = configDict;
+        this.dataSourceMap = new ConcurrentHashMap<>();
+    }
 
     public DataSource get(DatabaseUrlKey key) {
-
+        DataSource ds = dataSourceMap.get(key);
+        if (ds == null) {
+            synchronized (key.name().intern()) {
+                ds = dataSourceMap.get(key);
+                if (ds == null) {
+                    ds = new InnerDataSource(() -> configDict.get(key));
+                    dataSourceMap.put(key, ds);
+                }
+            }
+        }
+        return ds;
     }
 }
---
+++ src/main/java/com/zero/base/database/InnerDataSource.java
@@ -0,0 +1,71 @@
+package com.zero.base.database;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+/**
+ * 数据源代理
+ */
+public class InnerDataSource implements DataSource {
+    private final HikariDataSource dataSource;
+    private final Supplier<String> urlSupplier;
+
+    public InnerDataSource(Supplier<String> urlSupplier) {
+        this.dataSource = new HikariDataSource();
+        this.urlSupplier = urlSupplier;
+        this.dataSource.setJdbcUrl(urlSupplier.get());
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return dataSource.getConnection(username, password);
+    }
+
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return dataSource.unwrap(iface);
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return dataSource.isWrapperFor(iface);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return dataSource.getLogWriter();
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        dataSource.setLogWriter(out);
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        dataSource.setLoginTimeout(seconds);
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return dataSource.getLoginTimeout();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return dataSource.getParentLogger();
+    }
+}
